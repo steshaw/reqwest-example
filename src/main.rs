007c66b75eb815b2fe869d2938eedbda2cb35ddb
Steven Shaw <steven@steshaw.org>
2021-12-12 15:30:23 +1000

    eyre

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-async fn get_printer_fact() -> Result<String, reqwest::Error> {
-    let resp = reqwest::get("https://printerfacts.cetacean.club/fact").await?;
+use eyre::{Result, WrapErr};
 
-    resp.text().await
+async fn get(url: &str) -> Result<String> {
+    let resp = reqwest::get(url).await?;
+
+    resp.text().await.wrap_err(format!("Failed to get url={}", url))
+}
+
+async fn get_printer_fact() -> Result<String> {
+    get("https://printerfacts.cetacean.club/fact").await
 }
 
 #[tokio::main]
@@ -11,4 +17,10 @@ async fn main() {
         Ok(f) => println!("Printer fact: {}", f),
         Err(err) => println!("Argh: {}", err),
     }
+
+    let broken = get("broken").await;
+    println!("broken = {:?}", broken);
+
+    let broken2 = get("http://127.0.0.1/argh").await;
+    println!("broken2 = {:?}", broken2);
 }
