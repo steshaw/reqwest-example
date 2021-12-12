521695620f631d1e38b29e504785dab24e38e0a6
Steven Shaw <steven@steshaw.org>
2021-12-12 11:16:08 +1000

    reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+async fn get_printer_fact() -> Result<String, reqwest::Error> {
+    let resp = reqwest::get("https://printerfacts.cetacean.club/fact").await?;
+
+    resp.text().await
+}
+
+#[tokio::main]
+async fn main() {
+    get_printer_fact().await.into_iter().for_each(|f| {
+        println!("your printer fact is: {}", f);
+    });
 }
